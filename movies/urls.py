73cbf4f0e46fe49241c04cd3d403639a82a9d8d3
NMP <nmp24976@gmail.com>
2026-04-14 22:49:12 +0530

    Connected movies urls to users_app folder

--- movies/urls.py
+++ movies/urls.py
@@ -1,10 +1,10 @@
 
 from django.urls import path
-from . import views
+from users_app import views  # Seedha users_app ke views uthao
 
 app_name = 'movies' # Isse Django ko pata chalta hai ki ye movies app hai
 
 urlpatterns = [
     # Ye wo link hai jo video player ko kholega
-    path('watch/<int:movie_id>/', views.watch_movie, name='watch_movie'),
+    path('watch/<int:movie_id>/',views.watch_movie, name='watch_movie'),
 ]
