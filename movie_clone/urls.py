928fc25e08bcc19d4bf3e0f0472f26c730096fc9
NMP <nmp24976@gmail.com>
2026-04-14 22:01:30 +0530

    Fixed movies path and media urls

--- movie_clone/urls.py
+++ movie_clone/urls.py
@@ -5,17 +5,20 @@ from django.conf.urls.static import static
 from django.shortcuts import redirect
 
 urlpatterns = [
-    # 1. Admin Panel
+    # 1. Admin Panel (Ye pehle se hai, isse rehne dein)
     path('admin/', admin.site.urls),
     
-    # 2. Users App (Aapke purane saare functions yahan safe hain)
+    # 2. Users App
     path('users/', include('users_app.urls')),
     
-    # 3. Home Redirect
+    # 3. Movies App (Ye line missing thi, isse movies load hongi)
+    path('movies/', include('movies.urls')), 
+    
+    # 4. Home Redirect (Jab koi seedha website khole toh wo users/ par jaye)
     path('', lambda request: redirect('users/', permanent=False)),
 ]
 
-# Media aur Static files ki purani settings
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# --- Sabse Zaroori Badlav (Media Files ke liye) ---
+# Yahan se 'if settings.DEBUG' hata diya hai taaki Render par video chale
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
